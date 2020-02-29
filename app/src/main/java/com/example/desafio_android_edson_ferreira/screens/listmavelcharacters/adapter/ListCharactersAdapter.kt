0bebb6587de9b7bf3aa83a1063aacaea3730dbc9
Edson Ferreira <edferreira3005@gmail.com>
2020-02-29 18:24:59 -0300

    iniciando config do adapter e adicionando constantes de chaves de acesso a API

--- app/src/main/java/com/example/desafio_android_edson_ferreira/screens/listmavelcharacters/adapter/ListCharactersAdapter.kt
+++ app/src/main/java/com/example/desafio_android_edson_ferreira/screens/listmavelcharacters/adapter/ListCharactersAdapter.kt
@@ -1,5 +1,31 @@
 package com.example.desafio_android_edson_ferreira.screens.listmavelcharacters.adapter
 
-class ListCharactersAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.desafio_android_edson_ferreira.R
 
+class ListCharactersAdapter :
+    RecyclerView.Adapter<ListCharactersAdapter.ListCharactersViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListCharactersViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
+        return ListCharactersViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListCharactersViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+    class ListCharactersViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+    }
 }
---
+++ app/src/main/java/com/example/desafio_android_edson_ferreira/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_android_edson_ferreira.utils
+
+class Constants {
+    var PUBLIC_KEY = "38559a7b2ca531de20bcd5a0d910781a"
+    var PRIVATE_KEY = "158418a3da2963aaf56640f9e22a1ed93a448716"
+}
